c7ae0c0fdfb05b04a5bd8d26939741c331d41483
Toopy <florent@toopy.org>
2018-05-31 15:54:51 +0200

    Update urls definition in example project

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,12 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include
+from django.conf.urls import url
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
+urlpatterns = [
     url(r'^$', 'subapp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-)
+]
